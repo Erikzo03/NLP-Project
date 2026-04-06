34720fad6f358141ee762ce6341544ac4626e33f
O´NEAL <oneal.pa.okutu@outlook.com>
2026-04-06 20:51:40 +0200

    eperiment on the trainign

--- Baselines/train_norwegian_baseline.py
+++ Baselines/train_norwegian_baseline.py
@@ -1,14 +1,16 @@
+import os
 from train_ner_baseline import main
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
     main(
-        {
-            "train_file": "Datasets/Norwegian/nno_norne-ud-train.iob2",
-            "dev_file": "Datasets/Norwegian/nno_norne-ud-dev.iob2",
-            "test_file": "Datasets/Norwegian/nno_norne-ud-test.iob2",
-            "model_name": "bert-base-multilingual-cased",
-            "output_dir": "outputs/norwegian_bert_baseline",
-            "label_report_path": "Baselines/reports/norwegian_labels.json",
+        {"train_file": os.path.join(BASE_DIR, "Datasets/Norwegian/nno_norne-ud-train.iob2"),
+        "dev_file": os.path.join(BASE_DIR, "Datasets/Norwegian/nno_norne-ud-dev.iob2"),
+        "test_file": os.path.join(BASE_DIR, "Datasets/Norwegian/nno_norne-ud-test.iob2"),
+        "model_name": "bert-base-multilingual-cased",
+        "output_dir": os.path.join(BASE_DIR, "outputs/norwegian_bert_baseline"),
+        "label_report_path": os.path.join(BASE_DIR, "Baselines/reports/norwegian_labels.json"),
+            
         }
     )
